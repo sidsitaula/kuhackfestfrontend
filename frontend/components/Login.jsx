9fe3772e1a30a0620ea9de24759b3d519a1be663
Mukul <mukul.development@gmail.com>
2022-08-05 15:05:05 +0545

    Login Right Panel Complete

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -8,6 +8,10 @@ const Login = () => {
 
     let teacherButtonStyle = currentLogin === "teacher"? styles.teacherButton : styles.disabledButton;
     let studentButtonStyle = currentLogin === "student"? styles.studentButton : styles.disabledButton;
+    let loginButtonStyle =
+      currentLogin === "student"
+        ? { backgroundColor: "#bc2aa9" }
+        : { backgroundColor: "#2a60bc" };
 
     const teacherButtonToggle = ()=>{
         setCurrentLogin("teacher");
@@ -16,12 +20,21 @@ const Login = () => {
     const studentButtonToggle = ()=>{
         setCurrentLogin("student");
     }
+
+    const handleSignUp = ()=>{
+        alert("Sign Up")
+    }
     
   return (
     <div className={styles.mainBody}>
       <section className={styles.leftPanel}>Left</section>
 
       <section className={styles.rightPanel}>
+        <section className={styles.loginHead}>
+          <h1>{"Hello Again!"}</h1>
+          <h2>{"Please Login With Your Credentials"}</h2>
+        </section>
+
         <section className={styles.buttonRow}>
           <button className={teacherButtonStyle} onClick={teacherButtonToggle}>
             Teacher
@@ -31,9 +44,15 @@ const Login = () => {
           </button>
         </section>
 
-        <section className={styles.loginHead}>
-            <h1>Hello Again</h1>
-        </section>
+        <form className={styles.loginForm}>
+          <input type="email" placeholder={"Enter Your Email"} />
+          <input type="password" placeholder={"Enter Your Password"} />
+          <button style={loginButtonStyle}>Login</button>
+        </form>
+
+        <h2 className={styles.footer}>
+          Don't have an account yet? <span onClick={handleSignUp} className={styles.signUp}>Sign Up</span>
+        </h2>
       </section>
     </div>
   );
